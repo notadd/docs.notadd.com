5ce65ece7db5829c07c54d6aaf467b80c95f7943
twilroad <269044570@qq.com>
2018-03-06 12:24:19 +0800

    add address info after server up

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const ip = require("ip");
 
 const app = new express();
 
 app.use(express.static(process.cwd()));
 
 app.listen(4000);
+
+console.log(`Servering on: http://${ip.address()}:4000`);
