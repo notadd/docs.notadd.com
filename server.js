57a7138715198d9d0d8249af26bb393f0085c329
twilroad <269044570@qq.com>
2018-04-08 14:00:31 +0800

    feat(server): add ability to open url in browser

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const ip = require("ip");
+const { openInBrowser } = require("@already/open-in-browser");
 
 const app = new express();
 
 app.use(express.static(process.cwd()));
 
 app.listen(4000);
+
+openInBrowser(`http://${ip.address()}:4000`);
 
 console.log(`Servering on: http://${ip.address()}:4000`);
